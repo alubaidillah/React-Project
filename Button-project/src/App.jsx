0d9fd2113b7065c1debd490e3b41a422f5faf903
Alubaidillah <alubaidillah1@gmail.com>
2025-05-07 09:14:12 +0700

    menambahkan prop-types

--- Button-project/src/App.jsx
+++ Button-project/src/App.jsx
@@ -1,13 +1,11 @@
-import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
 
 
 
 function App(){
   return(
   <>
-  <Student name="Spongebob" age={30} isStudent={false}/>
-  <Student name="Patric" age={24} isStudent={true} ></Student>
-  <Student name="Squidward" age={30} isStudent={true}></Student>
+  <UserGreeting isLoggedIn={false} username="alubai" />
   </> 
   );
 } 
