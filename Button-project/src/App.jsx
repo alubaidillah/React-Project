4389d223ff8e67a8a6f23239062f0221491cc1f2
Alubaidillah <alubaidillah1@gmail.com>
2025-05-07 08:50:04 +0700

    menyelesaikan pelatihan student

--- Button-project/src/App.jsx
+++ Button-project/src/App.jsx
@@ -1,11 +1,15 @@
-import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 
 
 function App(){
   return(
-  <Button/>
+  <>
+  <Student name="Spongebob" age={30} isStudent={false}/>
+  <Student name="Patric" age={24} isStudent={true} ></Student>
+  <Student name="Squidward" age={30} isStudent={true}></Student>
+  </> 
   );
-}
+} 
 
 export default App;
